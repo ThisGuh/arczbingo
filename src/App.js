13eb3eee023c17e489a92bf945c8adcfc661ec04
ThisGuh <jakiepolska1111@gmail.com>
2019-12-16 22:41:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {}
+  render() {
+    return (
+      <div className="App">
+        <h1>Bingo</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td><p>*Patrzenie przez okno*</p></td>
+              <td><p>Wstawiam ci uwage</p></td>
+              <td><p>Code Bloks</p></td>
+              <td><p>Ten w czarnym</p></td>
+              <td><p>Szisza</p></td>
+            </tr>
+            <tr>
+              <td><p>*Projektor sam się włącza*</p></td>
+              <td><p>Wstawiam ci jedynke</p></td>
+              <td><p>*Tapeta w pieski*</p></td>
+              <td><p>Kto chce odpowiadać?</p></td>
+              <td><p>Aaa</p></td>
+            </tr>
+            <tr>
+              <td><p>Fajne jest</p></td>
+              <td><p>No nie</p></td>
+              <td><p>Pacie się</p></td>
+              <td><p>Wyłączcie te dźwięki</p></td>
+              <td><p>Ćśśś</p></td>
+            </tr>
+            <tr>
+              <td><p>Kto to powiedział?</p></td>
+              <td><p>*Poker face*</p></td>
+              <td><p></p></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
